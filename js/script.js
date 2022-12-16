f867f6de5353cab41e28528efaae8c408fba1d8b
ilario-moreni <ilario2001.mor@gmail.com>
2022-12-16 15:43:28 +0100

    bonus

--- js/script.js
+++ js/script.js
@@ -3,7 +3,8 @@ const {createApp} = Vue;
 createApp({
     data() {
         return {
-            message: 'Hello Vue!'
+            message: 'Hello Vue!',
+            img: 'https://i.picsum.photos/id/24/4855/1803.jpg?hmac=ICVhP1pUXDLXaTkgwDJinSUS59UWalMxf4SOIWb9Ui4'
         }
     }
 }).mount('#app')
